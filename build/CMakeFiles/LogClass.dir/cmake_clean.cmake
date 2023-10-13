6d3e936711cf8884b55336f85b63b89a6da25407
MorganJBradford <morganjbradford95@gmail.com>
2023-10-13 10:09:26 -0500

    initial commit

---
+++ build/CMakeFiles/LogClass.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LogClass.dir/Main.cpp.o"
+  "CMakeFiles/LogClass.dir/Main.cpp.o.d"
+  "LogClass"
+  "LogClass.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LogClass.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/LogClass.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for LogClass.
+# This may be replaced when dependencies are built.
